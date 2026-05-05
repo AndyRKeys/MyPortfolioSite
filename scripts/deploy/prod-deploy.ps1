8c16482ffed399b5f684d85b06c5c07ecc6d7849
Andy Keys <andy.r.keys@outlook.com>
2026-05-05 13:31:18 +0100

    hotfix: prod deploy script

--- scripts/deploy/prod-deploy.ps1
+++ scripts/deploy/prod-deploy.ps1
@@ -1,4 +1,4 @@
 # Trigger a production deploy from Windows via SSH.
 # Connects to the Pi and runs scripts/deploy/prod-deploy.sh remotely.
-param([string]$Hostname = 'raspberrypi3.local')
+param([string]$Hostname = 'raspberrypi3')
 ssh $Hostname "bash ~/MyPortfolioSite/scripts/deploy/prod-deploy.sh"
